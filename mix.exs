c0246d738f2c017d81006e647b10896429e7cea4
tomhoenderdos <hoenderdos.t@hsleiden.nl>
2026-06-02 21:51:33 +0200

    Bump MatterEx version to 0.3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MatterEx.MixProject do
   def project do
     [
       app: :matter_ex,
-      version: "0.2.0",
+      version: "0.3.0",
       elixir: "~> 1.17",
       description: "A Matter (smart home) protocol stack in pure Elixir",
       start_permanent: Mix.env() == :prod,
